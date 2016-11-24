accdefa8de630c11955ebded7d754efec2daa5c7
Francois62 <franois.andrzejewski@laposte.net>
2016-11-24 11:36:53 +0100

    start

---
+++ js/jsword.js
@@ -0,0 +1,30 @@
+//var words = [];
+var voy =[a, e, i, o, u, y];
+voy = true;
+var len = mot.length;
+//var i=0;
+var mot = document.getElementById("word");
+mot.style.color="navy";
+document.write(mot.bold());
+len = words.length;
+while(len<7){
+  console.log(words[i]);
+  mot.toUpperCase();
+//   document.getElementById("word").innerHTML=
+  i++;
+  else(){
+  mot.toLowerCase();
+  }
+}
+
+for (len = len-1; j--){
+
+}
+function count(){
+for (voy==false){
+   return
+}
+else(){
+
+}
+}
